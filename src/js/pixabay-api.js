0d0a5d1ed1c2ab0aba311dbb30b3f8f3a90eaac5
MaksymSelezen <foqney@gmail.com>
2024-11-10 19:21:33 +0200

    js_hw_11

---
+++ src/js/pixabay-api.js
@@ -0,0 +1,19 @@
+const BASE_URL = 'https://pixabay.com/api/';
+const API_KEY = '46793755-9d28ad9f5c835a0a0339cf9e7';
+
+export function fetchImages(query) {
+  const params = new URLSearchParams({
+    key: API_KEY,
+    q: query,
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: 'true',
+  });
+
+  return fetch(`${BASE_URL}?${params}`).then(response => {
+    if (!response.ok) {
+      throw new Error(response.statusText);
+    }
+    return response.json();
+  });
+}
